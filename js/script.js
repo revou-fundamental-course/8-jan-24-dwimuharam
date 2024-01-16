e51b6e8b02ac90fb8690add86709fd27c1b7c106
unknown <dwimuharam11@hmail.com>
2024-01-17 00:09:03 +0700

    Update Javascript

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function calculateLuas() {
   // Menghitung luas persegi
   var area = luasSquareSide * luasSquareSide;
 
-  // Menampilkan hasil di elemen dengan id 'result'
+  // Menampilkan hasil di elemen dengan id 'luasResult'
   document.getElementById("luasResult").innerText = "Luas Persegi: " + area;
 }
 
@@ -13,20 +13,22 @@ function calculateKeliling() {
   //Mengambil nilai panjang sisi dari input
   var kelilingSquareSide = document.getElementById("kelilingSquareSide").value;
 
-  // Menghitung luas persegi
+  // Menghitung Keliling persegi
   var area = 4 * kelilingSquareSide;
 
-  // Menampilkan hasil di elemen dengan id 'result'
+  // Menampilkan hasil di elemen dengan id 'kelilingResult'
   document.getElementById("kelilingResult").innerText =
     "Keliling Persegi: " + area;
 }
 
+//Tombol Reset Luas Persegi
 function resetLuasCalculator() {
   // Mengatur nilai input dan hasil menjadi kosong
   document.getElementById("luasSquareSide").value = "";
   document.getElementById("luasResult").innerText = "Luas Persegi: ";
 }
 
+//Tombol Reset Keliling Persegi
 function resetKelilingCalculator() {
   // Mengatur nilai input dan hasil menjadi kosong
   document.getElementById("kelilingSquareSide").value = "";
